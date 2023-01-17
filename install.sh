fa8e0fe4f03a20e9515030df609bd5c4f738dbad
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-01-17 22:20:45 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# PACKAGES
+sudo apt install git curl wget jq ripgrep tmux docker.io docker-compose
+
+# NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# NODE
+nvm install v18.13.0
+npm i -g nodemon
+npm i -g prettier
+
+# NEOVIM
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+tar xvzf nvim-linux64.tar.gz
+mkdir -p ~/.local
+cp -r nvim-linux64/* ~/.local
+rm nvim-linux64.tar.gz
+rm -rf nvim-linux64
+
+# PLUG
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# COPY CONFIGS
+mkdir -p ~/.config/nvim
+cp init.vim ~/.config/nvim
+cp .tmux.conf ~/
+
+# FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
